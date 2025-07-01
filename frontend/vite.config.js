3b1310ad0080030d5918026b8285141be8490b76
Ankit Sangwan <ankitsangwan0705@gmail.com>
2025-07-01 06:12:45 +0530

    config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    // 🔥 Important for SPA routing (especially in dev)
-    historyApiFallback: true,
-  },
-});
+  plugins: [react(),tailwindcss()],
+})
